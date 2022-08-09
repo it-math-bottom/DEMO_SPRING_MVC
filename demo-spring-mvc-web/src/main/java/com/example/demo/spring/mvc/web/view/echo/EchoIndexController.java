44794f95e395a4a17c2ed27e986c23c261fff2df
it-math-bottom <free.min1123t@gmail.com>
2022-08-09 17:37:51 +0900

    入力画面用jsp側へ遷移する際に空FormをModelに登録

--- demo-spring-mvc-web/src/main/java/com/example/demo/spring/mvc/web/view/echo/EchoIndexController.java
+++ demo-spring-mvc-web/src/main/java/com/example/demo/spring/mvc/web/view/echo/EchoIndexController.java
@@ -3,8 +3,9 @@ package com.example.demo.spring.mvc.web.view.echo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.ui.Model;
 import com.example.demo.spring.mvc.constant.ViewUri.Echo;
+import com.example.demo.spring.mvc.web.tododelete.EchoForm;
 
 @Controller
 @RequestMapping(Echo.INDEX)
@@ -13,7 +14,9 @@ public class EchoIndexController {
 	private static final String VIEW_URL = Echo.INDEX + "/index";
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String doGetIndex() {
+	public String doGetIndex(Model model) {
+		EchoForm echoForm = new EchoForm();
+		model.addAttribute(echoForm);
 		return VIEW_URL;
 	}
 }
