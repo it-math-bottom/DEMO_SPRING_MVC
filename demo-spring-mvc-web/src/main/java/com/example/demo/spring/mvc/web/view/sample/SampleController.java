934bb73f820934a8a2ae0495d9085791b99ba9e1
it-math-bottom <free.min1123t@gmail.com>
2022-08-09 15:37:36 +0900

    RequestMappingをControllerに付与

--- demo-spring-mvc-web/src/main/java/com/example/demo/spring/mvc/web/view/sample/SampleController.java
+++ demo-spring-mvc-web/src/main/java/com/example/demo/spring/mvc/web/view/sample/SampleController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.demo.spring.mvc.constant.ViewUri.Sample;;
 
 @Controller
+@RequestMapping(Sample.INDEX)
 public class SampleController {
 
-	private static final String TEST_URL = Sample.INDEX;
-	private static final String TEST_VIEW_URL = Sample.INDEX + "/index";
+	private static final String VIEW_URL = Sample.INDEX + "/index";
 	
-	@RequestMapping(value = TEST_URL, method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String doGetIndex() {
-		return TEST_VIEW_URL;
+		return VIEW_URL;
 	}
 }
