4f8f6dccfcb04ea74351d464da549a22f1bea91c
it-math-bottom <free.min1123t@gmail.com>
2022-08-09 16:27:48 +0900

    Formクラス作成

---
+++ demo-spring-mvc-web/src/main/java/com/example/demo/spring/mvc/web/tododelete/EchoForm.java
@@ -0,0 +1,14 @@
+package com.example.demo.spring.mvc.web.tododelete;
+
+import java.io.Serializable;
+
+public class EchoForm implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String inputText;
+	
+	public String getInutText() {
+		return inputText;
+	}
+}
