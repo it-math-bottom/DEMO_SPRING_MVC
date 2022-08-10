229dc6d64b12500318d39b089a30a5d7525dde65
it-math-bottom <free.min1123t@gmail.com>
2022-08-11 01:33:34 +0900

    EchoControllerに共通Controllerを付与

--- demo-spring-mvc-web/src/main/java/com/example/demo/spring/mvc/web/view/echo/EchoController.java
+++ demo-spring-mvc-web/src/main/java/com/example/demo/spring/mvc/web/view/echo/EchoController.java
@@ -5,27 +5,42 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.validation.BindingResult;
 import com.example.demo.spring.mvc.constant.ViewUri.Echo;
 import com.example.demo.spring.mvc.domain.form.EchoForm;
+import com.example.demo.spring.mvc.web.reqhandler.controller.AbstractController;
+import com.example.demo.spring.mvc.web.reqhandler.process.AbstractViewProcess;
 
 @Controller
 @RequestMapping(Echo.ECHO)
-public class EchoController {
+public class EchoController extends AbstractController {
 
 	private static final String INPUT_VIEW_URL = Echo.ECHO + "/input";
 	private static final String OUTPUT_VIEW_URL = Echo.ECHO + "/output";
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String doGet(final EchoForm echoForm) {
-		return INPUT_VIEW_URL;
+	public ModelAndView doGet(final EchoForm echoForm) {
+		return super.handleRequest(echoForm, null, new AbstractViewProcess<EchoForm>() {
+			@Override
+			public ModelAndView onMainProcess() {
+				return new ModelAndView(INPUT_VIEW_URL);
+			}
+		});
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String doPost(@Valid final EchoForm form, final BindingResult result) {
-		if (result.hasErrors()) {
-			return INPUT_VIEW_URL;
-		}
-		return OUTPUT_VIEW_URL;
+	public ModelAndView doPost(@Valid final EchoForm form, final BindingResult result) {
+		return super.handleRequest(form, result, new AbstractViewProcess<EchoForm>() {
+			@Override
+			public ModelAndView onMainProcess() {
+				return new ModelAndView(OUTPUT_VIEW_URL);
+			}
+			
+			@Override
+			public ModelAndView onValidationError() {
+				return new ModelAndView(INPUT_VIEW_URL);
+			}
+		});
 	}
 }
